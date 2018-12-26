990b4701c7f7b992e5d460de9fbf96f7715ea33b
Sergei L <srglbnv@gmail.com>
2018-12-26 16:02:34 +0400

    Push user agent updates to git

--- updater/updater.js
+++ updater/updater.js
@@ -1,18 +1,20 @@
+const fs = require('fs').promises
+const { promisify } = require('util')
+const rimraf = promisify(require('rimraf'))
 const got = require('got')
 const UAParser = require('ua-parser-js')
 const compareVersions = require('compare-versions')
+const simpleGit = require('simple-git/promise')
 
 module.exports = class Updater {
   static async init () {
     const updater = new this()
-    const agents = await updater.crawl()
-    const latest = updater.getActualAgents(agents)
-    console.log(latest)
+    updater.run()
   }
 
   constructor () {
-    this.delay = 1000
-    this.pages = 3
+    this.delay = 1
+    this.pages = 4
     this.config = {
       windows: {
         url: 'https://developers.whatismybrowser.com/useragents/explore/operating_system_name/windows/',
@@ -45,6 +47,61 @@ module.exports = class Updater {
         ]
       }
     }
+  }
+
+  async run () {
+    const agents = await this.crawl()
+    const actual = this.getActualAgents(agents)
+    const path = await this.cloneRepo()
+    let current = await fs.readFile(path + 'src/agents.json')
+    current = JSON.parse(current)
+    if (this.merge(current, actual)) {
+      current = JSON.stringify(current, null, 2)
+      await fs.writeFile(path + 'src/agents.json', current)
+      await this.pushRepo(path)
+    }
+  }
+
+  merge (currents, actuals) {
+    let merged = false
+    actuals.forEach(actual => {
+      let current = currents.find(current => (
+        current.os === actual.os && current.browser === actual.browser
+      ))
+      if (!current) {
+        currents.push(actual)
+        merged = true
+      } else if (compareVersions(actual.version, current.version) === 1) {
+        current.version = actual.version
+        current.ua = actual.ua
+        merged = true
+      }
+    })
+
+    return merged
+  }
+
+  async pushRepo (path) {
+    await fs.writeFile('/tmp/.deploy_key',
+      Buffer.from(process.env.DEPLOY_KEY, 'base64'))
+    const GIT_SSH_COMMAND = 'ssh -i /tmp/.deploy_key'
+
+    const git = simpleGit(path)
+    await git.env({ ...process.env, GIT_SSH_COMMAND })
+    await git.add('.')
+    await git.commit('Update user agent strings')
+    await git.push('origin', 'master')
+  }
+
+  async cloneRepo () {
+    const remote = 'git@github.com:srgl/ua-smart-switcher.git'
+    const path = '/tmp/.repo/'
+    await rimraf(path)
+    await fs.mkdir(path)
+
+    const git = simpleGit(path)
+    await git.clone(remote, path)
+    return path
   }
 
   async crawl () {
